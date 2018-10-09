1a7ff7b3f6ee4d6a301ea1f83139b5b86e54b2f1
Dorota Sarba <dorota.sarba@gmail.com>
2018-10-09 11:04:49 +0200

    crate a new repository

---
+++ js/main.js
@@ -0,0 +1,11 @@
+document.getElementById("link1").addEventListener('click', function(e){
+    alert('Strona w przygotowaniu!');
+});
+
+document.getElementById("link2").addEventListener('click', function(e){
+    alert('Strona w przygotowaniu!');
+});
+
+document.getElementById("link3").addEventListener('click', function(e){
+    alert('Strona w przygotowaniu!');
+});
